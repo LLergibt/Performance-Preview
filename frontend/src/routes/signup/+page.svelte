8401359a41262aa66ba6e77eb1f7b29cecf1ebd2
Murik0-0 <murkaunezhev@icloud.com>
2025-10-22 23:32:41 +0300

    style(auth): Изменили стилизацию окна регистрации.

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -3,55 +3,51 @@
  import { Label } from "$lib/components/ui/label/index.js";
  import { Input } from "$lib/components/ui/input/index.js";
  import * as Card from "$lib/components/ui/card/index.js";
+ import '../../app.css';
 </script>
 
-<div class="flex items-center justify-center min-h-screen">
-<Card.Root class="w-full max-w-sm">
- <Card.Header>
-  <Card.Title>Регистрация</Card.Title>
-  <Card.Description
-   >Введите свою почту, чтоб зарегистрировать аккаунт</Card.Description
-  >
-  <Card.Action>
-   <Button variant="link"><a href="/signin">Sign In</a></Button>
-  </Card.Action>
- </Card.Header>
- <Card.Content>
-  <form>
-   <div class="flex flex-col gap-6">
-    <div class="grid gap-2">
-     <Label for="firstname">Имя</Label>
-     <Input id="firstname" type="firstname" placeholder="Имя" required />
+<div>
+    <div class="flex items-center justify-center min-h-screen bg-[#F7F6F2]">
+        <Card.Root class="py-30 px-0 w-full max-w-lg">
+            <Card.Header class="text-center">
+                <Card.Title class="text-center text-2xl">Регистрация</Card.Title>
+                <Card.Description class="text-sm text-[#71717a]">Добавьте свои данные для создания аккаунта</Card.Description>
+            </Card.Header>
+
+            <Card.Content class="w-full">
+                <form>
+                <div class="flex flex-col gap-2 w-full">
+                    <!--<div class="grid gap-2">
+                        <Input id="lastname" type="text" placeholder="Фамилия" class="w-full" required />
+                    </div>-->
+                    <div class="grid gap-2">
+                        <Input id="name" type="text" placeholder="Иванов Иван Иванович" class="w-full" required />
+                    </div>
+                    <div class="grid gap-2">
+                        <Input id="role" type="text" placeholder="Роль" class="w-full" required />
+                    </div>
+                    <div class="grid gap-2">
+                        <Input id="email" type="email" placeholder="Name@gmail.com" class="w-full" required />
+                    </div>
+                    <div class="grid gap-2 flex items-center">
+                        <Input id="password" type="password" placeholder="Пароль" class="w-full" required />
+                    </div>
+                    <div class="grid gap-2 flex items-center">
+                        <Input id="confirm-password" type="password" placeholder="Подтвердите пароль" class="w-full" required />
+                    </div>
+                </div>
+                </form>
+            </Card.Content>
+
+            <Card.Footer class="flex-col gap-2 w-full">
+                <Button type="submit" class="w-full">Создать аккаунт</Button>
+            </Card.Footer>
+
+            <div class="flex items-center justify-center">
+                <Card.Action>
+                <Label for="account" class="text-[#8c8c8c]">Уже есть аккаунт?<Button variant="link" class="p-0 m-0"><a href="/signin" class="text-[#8c8c8c] underline">Войти</a></Button></Label>
+                </Card.Action>
+            </div>
+        </Card.Root>
     </div>
-    <div class="grid gap-2">
-     <Label for="lastname">Фамилия</Label>
-     <Input id="lastname" type="lastname" placeholder="Фамилия" required />
-    </div>
-    <div class="grid gap-2">
-     <Label for="surname">Отчество</Label>
-     <Input id="surname" type="surname" placeholder="Отчество" required />
-    </div>
-    <div class="grid gap-2">
-     <Label for="email">Email</Label>
-     <Input id="email" type="email" placeholder="m@example.com" required />
-    </div>
-    <div class="grid gap-2">
-     <div class="flex items-center">
-      <Label for="password">Password</Label>
-     </div>
-     <Input id="password" type="password" required />
-    </div>
-    <div class="grid gap-2">
-     <div class="flex items-center">
-      <Label for="password">Confirm Password</Label>
-     </div>
-     <Input id="password" type="password" required />
-    </div>
-   </div>
-  </form>
- </Card.Content>
- <Card.Footer class="flex-col gap-2">
-  <Button type="submit" class="w-full">Зарегистрироваться</Button>
- </Card.Footer>
-</Card.Root>
 </div>
